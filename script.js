22a84b884363657f15cdd2092ad4ef2162fc712b
darkie-75coder <shivanshkumargupta75@gmail.com>
2026-01-07 05:45:31 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+let face = document.querySelector("img");
+let h1 = document.querySelector("h1");
+let body = document.querySelector("body");
+let dclicks = document.querySelector(".clicks");
+let count = document.querySelector(".click");
+let hagal = document.querySelector(".hagal");
+let one = document.querySelector(".main");
+let two = document.querySelector(".records");
+let clicks = 0;
+
+const audio = new Audio("mouse-click-sound.wav");
+const audio2 = new Audio("smile-dog-jumpscare-167171.wav");
+
+one.classList.remove("main");
+two.classList.remove("records");
+
+face.addEventListener("click", () => {
+  clicks += 1;
+  dclicks.innerText = clicks;
+  audio.pause();
+  audio.currentTime = 0;
+  audio.play();
+
+  face.classList.remove("hovet");
+  setTimeout(() =>{
+    face.classList.add("hovet");
+  }, 100)
+
+  setTimeout(() => {
+    face.classList.remove("pressed");
+  }, 70)
+
+  if (clicks >= 100 && clicks <= 199) {
+    face.src = "2.svg";
+    body.style.background = "url('b.svg')"
+  } 
+    else if (clicks >= 200 && clicks <= 299) {
+    face.src = "3.svg";
+    body.style.background = "url('c.svg')"
+  } 
+    else if (clicks >= 300 && clicks <= 399) {
+    face.src = "4.svg";
+    body.style.background = "url('d.svg')"
+  } 
+    else if (clicks >= 400 && clicks <= 499) {
+    face.src = "5.svg";
+    body.style.background = "url('e.svg')"
+  } 
+    else if (clicks >= 500 && clicks <= 599) {
+    face.src = "6.svg";
+    body.style.background = "url('f.svg')"
+  } 
+    else if (clicks >= 600 && clicks <= 699) {
+    face.src = "7.svg";
+    body.style.background = "url('g.svg')"
+  } 
+    else if (clicks >= 700 && clicks <= 799) {
+    face.src = "8.svg";
+    body.style.backgroundImage = "url('h.svg')";
+    body.style.backgroundRepeat = "no-repeat";
+    count.style.color = "#fff";
+    h1.style.color = "#fff";
+    body.style.backgroundSize = "cover";
+  }
+   else if (clicks >= 800 && clicks <= 899) {
+    face.src = "9.svg";
+    body.style.backgroundImage = "url('i.svg')";
+    body.style.backgroundSize = "cover";
+  }
+   else if (clicks >= 900 && clicks <= 999) {
+    face.src = "10.svg";
+    body.style.background = "url('j.svg')";
+    count.style.color = "#fff";
+    h1.style.color = "#fff";
+  }
+  else if (clicks >= 1000) {
+    count.style.color = "#000";
+    h1.style.color = "#000";
+    face.style.transform = "scale(3)";
+    audio2.play();
+  }
+});
